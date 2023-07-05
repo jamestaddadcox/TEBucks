77a2aa316fdd7230f49b87eb86c2e25596fe3db1
dovid creeger <dovidcreeger@gmail.com>
2023-07-05 11:54:31 -0400

    message

--- database/tebucks.sql
+++ database/tebucks.sql
@@ -1,6 +1,8 @@
 BEGIN TRANSACTION;
 
 DROP TABLE IF EXISTS users;
+DROP TABLE IF EXISTS account;
+DROP TABLE IF EXISTS transfer;
 
 CREATE TABLE users (
 	user_id serial NOT NULL,
@@ -12,6 +14,19 @@ CREATE TABLE users (
 	role varchar(20),
 	CONSTRAINT pk_users PRIMARY KEY (user_id),
 	CONSTRAINT uq_username UNIQUE (username)
+);
+CREATE TABLE account (
+    account_id serial NOT NULL,
+    user_id serial NOT NULL,
+    balance double NOT NULL,
+);
+CREATE TABLE transfer (
+    transfer_id serial NOT NULL,
+    to_user_id int NOT NULL,
+    from_user_id int NOT NULL,
+    type VARCHAR(50),
+    amount double NOT NULL,
+    status VARCHAR(50),
 );
 
 
